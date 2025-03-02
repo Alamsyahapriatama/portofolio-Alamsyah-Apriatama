7002083f3055494fbfaa94e23dbd6f346294e3eb
taufikmuharrom <taufikmuharromdev@gmail.com>
2025-03-03 01:12:54 +0700

    feat : integrate submit form api

--- components/sections/CallToAction.vue
+++ components/sections/CallToAction.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { reactive, ref, toRefs } from "vue";
-
-// Definisikan tipe untuk form
 interface FormData {
   nama: string;
   ttl: string;
@@ -13,7 +10,6 @@ interface FormData {
   motivasi: string;
 }
 
-// Data form dengan tipe `reactive`
 const form = reactive<FormData>({
   nama: "",
   ttl: "",
@@ -25,24 +21,19 @@ const form = reactive<FormData>({
   motivasi: "",
 });
 
-// Konversi reactive ke refs untuk binding
 const formRefs = toRefs(form);
-
-// State untuk validasi error
 const errors = ref<Partial<FormData>>({});
+const isLoading = ref(false);
 
-// Fungsi validasi
 const validateForm = async (): Promise<boolean> => {
   errors.value = {};
 
-  // Validasi setiap inputan
   (Object.keys(form) as Array<keyof FormData>).forEach((key) => {
     if (!form[key]) {
       errors.value[key] = "Kolom ini wajib diisi";
     }
   });
 
-  // Validasi khusus untuk nomor WhatsApp
   const whatsappPattern = /^[0-9]{10,15}$/;
   if (form.whatsapp && !whatsappPattern.test(form.whatsapp)) {
     errors.value.whatsapp =
@@ -50,30 +41,30 @@ const validateForm = async (): Promise<boolean> => {
   }
 
   await nextTick();
-
   return Object.keys(errors.value).length === 0;
 };
 
-// Fungsi submit form
 const submitForm = async (event: Event) => {
-  console.log("CLICK");
-  event.preventDefault(); // Hindari reload halaman
-
-  console.log("Trying to submit...");
+  event.preventDefault();
 
   if (await validateForm()) {
-    console.log("Form berhasil dikirim!", form);
-    alert("Form berhasil dikirim!");
-    form.nama = "";
-    form.ttl = "";
-    form.domisili = "";
-    form.universitas = "";
-    form.jurusan = "";
-    form.whatsapp = "";
-    form.sumberInfo = "";
-    form.motivasi = "";
-  } else {
-    console.log("Validasi gagal");
+    isLoading.value = true;
+    try {
+      const response = await fetch("https://api.ifc.web.id/event", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      });
+
+      if (!response.ok) throw new Error("Gagal mengirim data");
+
+      alert("Terima kasih sudah mendaftar! nantikan info selanjutnya.");
+      Object.keys(form).forEach((key) => (form[key as keyof FormData] = ""));
+    } catch (error) {
+      alert("Terjadi kesalahan. Silakan coba lagi.");
+    } finally {
+      isLoading.value = false;
+    }
   }
 };
 </script>
@@ -97,21 +88,19 @@ const submitForm = async (event: Event) => {
           </h2>
           <div class="mx-auto max-w-md sm:max-w-xl pt-10">
             <div class="space-y-5">
-              <!-- Input Nama -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">Nama</p>
                 <input
                   v-model="formRefs.nama.value"
                   type="text"
                   placeholder="Masukkan nama"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.nama" class="text-red-500 text-sm mt-1">
                   {{ errors.nama }}
                 </p>
               </div>
 
-              <!-- Input Tanggal Lahir -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">
                   Tanggal Lahir
@@ -119,27 +108,25 @@ const submitForm = async (event: Event) => {
                 <input
                   v-model="formRefs.ttl.value"
                   type="date"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.ttl" class="text-red-500 text-sm mt-1">
                   {{ errors.ttl }}
                 </p>
               </div>
 
-              <!-- Input Domisili -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">Domisili</p>
                 <input
                   v-model="formRefs.domisili.value"
                   type="text"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.domisili" class="text-red-500 text-sm mt-1">
                   {{ errors.domisili }}
                 </p>
               </div>
 
-              <!-- Input Universitas -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">
                   Asal Universitas
@@ -147,27 +134,25 @@ const submitForm = async (event: Event) => {
                 <input
                   v-model="formRefs.universitas.value"
                   type="text"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.universitas" class="text-red-500 text-sm mt-1">
                   {{ errors.universitas }}
                 </p>
               </div>
 
-              <!-- Input Jurusan -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">Jurusan</p>
                 <input
                   v-model="formRefs.jurusan.value"
                   type="text"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.jurusan" class="text-red-500 text-sm mt-1">
                   {{ errors.jurusan }}
                 </p>
               </div>
 
-              <!-- Input No WhatsApp -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">
                   No WhatsApp
@@ -176,14 +161,13 @@ const submitForm = async (event: Event) => {
                   v-model="formRefs.whatsapp.value"
                   type="text"
                   placeholder="Contoh: 08123456789"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.whatsapp" class="text-red-500 text-sm mt-1">
                   {{ errors.whatsapp }}
                 </p>
               </div>
 
-              <!-- Input Sumber Informasi -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">
                   Tahu info ini dari mana?
@@ -191,32 +175,35 @@ const submitForm = async (event: Event) => {
                 <input
                   v-model="formRefs.sumberInfo.value"
                   type="text"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500"
                 />
                 <p v-if="errors.sumberInfo" class="text-red-500 text-sm mt-1">
                   {{ errors.sumberInfo }}
                 </p>
               </div>
 
-              <!-- Input Motivasi -->
               <div>
                 <p class="text-left pb-2 text-white font-semibold">
                   Motivasi ikut acara ini
                 </p>
                 <textarea
                   v-model="formRefs.motivasi.value"
-                  class="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 h-24"
+                  class="w-full p-3 border rounded-md focus:ring-2 focus:ring-blue-500 h-24"
                 ></textarea>
                 <p v-if="errors.motivasi" class="text-red-500 text-sm mt-1">
                   {{ errors.motivasi }}
                 </p>
               </div>
 
-              <!-- Tombol Submit -->
               <button
                 @click="submitForm"
-                class="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-md font-semibold text-lg transition duration-300"
+                :disabled="isLoading"
+                class="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-md font-semibold text-lg transition duration-300 flex justify-center items-center"
               >
+                <span
+                  v-if="isLoading"
+                  class="animate-spin h-5 w-5 border-4 border-white border-t-transparent rounded-full mr-2"
+                ></span>
                 Daftar Sekarang
               </button>
             </div>
