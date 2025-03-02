d1a2b69baa7d87c9e6bcc920ebe5c5e6be0dbbf9
taufikmuharrom <taufikmuharromdev@gmail.com>
2025-03-02 17:06:40 +0700

    feat : setup project + adjust hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
 useHead({
-  title: 'Podux - Home',
+  title: "IT Fasting Camp",
   meta: [
-    { name: 'description', content: 'Podux Podcast landing page website.' },
+    {
+      name: "description",
+      content: "Tempat belajar IT Gratis.",
+    },
   ],
-  link: [
-    { rel: 'icon', type: 'image/png', href: '/logo.png' }
-  ],
-})
+  link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
+});
 </script>
 <template>
-    <div>
-        <SectionsHomeHeroSection/>
-        <SectionsHomeLatestPodcast/>
-        <SectionsHomePopularPodcasts/>
-        <SectionsAboutPodux/>
-        <SectionsWhyUs/>
-        <SectionsCallToAction/>
-    </div>
+  <div>
+    <SectionsHomeHeroSection />
+    <SectionsHomeLatestPodcast />
+    <SectionsHomePopularPodcasts />
+    <SectionsAboutPodux />
+    <SectionsWhyUs />
+    <SectionsCallToAction />
+  </div>
 </template>
